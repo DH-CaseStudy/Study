a022dfe2a9e7d940d81d993ea639a278bc74c5b7
이동휘 <dh.lee@salin.co.kr>
2024-07-07 22:36:40 +0900

    불투명 반환타입 / 익스텐션 추가

--- MyFirstPlayGround.playground/Pages/Class.xcplaygroundpage/Contents.swift
+++ MyFirstPlayGround.playground/Pages/Class.xcplaygroundpage/Contents.swift
@@ -1,5 +1,9 @@
 import Foundation
+import SwiftUI
+import PlaygroundSupport
 
+
+// # 클래스
 // 1. class 클래스이름: 부모클래스 {
 //    프로퍼티(변수)
 //    메서드(함수)
@@ -30,7 +34,7 @@ class Car{
         self.year = year
     }
     
-    func displayinfo(){
+    func displayInfo(){
         print("Car : \(brand) \(model) and year : \(year)")
     }
     
@@ -49,9 +53,9 @@ let myCar = Car(brand: "Hyundai", model: "Sonata", year: 2024)
 
 print(myCar.brand) // getter
 
-myCar.displayinfo()
+myCar.displayInfo()
 
-print(myCar.details)
+print(myCar.details) // getter
 
 myCar.details = "2011 Honda Civic" //setter
 
@@ -74,7 +78,7 @@ class Car2{
         self.year = year
     }
     
-    func displayinfo(){
+    func displayInfo(){
         print("Car : \(brand) \(model) and year : \(year)")
     }
     
@@ -153,3 +157,92 @@ elCar.displayinfo()
 // 오버라이딩 시 주의할 점
 // 1. 매개변수 타입 개수 동일
 // 2. 반환타입 동일
+
+
+// # 프로토콜
+// 규칙의 집합체
+// 준수한다 conform
+
+protocol Vehicle {
+    // 3 개의 프로퍼티'
+    var brand : String {get}
+    var model : String {get}
+    var year : Int {get}
+    
+    // 1 개의 메서드
+    func displayInfo()
+}
+
+class Car4 : Vehicle{
+    var brand: String // 저장 프로퍼티
+    var model: String
+    var year: Int
+    
+    init(brand: String, model: String, year: Int){
+        self.brand = brand
+        self.model = model
+        self.year = year
+    }
+    
+    func displayInfo(){
+        print("Car : \(brand) \(model) and year : \(year)")
+    }
+    
+    deinit {
+        print("메모리에서 제거되었습니다.")
+    }
+}
+
+struct Bicycle: Vehicle {
+    // 3 개의 프로퍼티'
+    var brand : String
+    var model : String
+    var year : Int
+    
+    init(brand: String, model: String, year: Int){
+        self.brand = brand
+        self.model = model
+        self.year = year
+    }
+    
+    func displayInfo(){
+        print("Car : \(brand) \(model) and year : \(year)")
+    }
+}
+
+func CreateCar() -> some Vehicle {
+    return Bicycle(brand: "kia", model: "niro", year: 2024)
+}
+
+CreateCar()
+
+
+// 불투명 반환 타입 opaque return type (some)
+// SwiftUI
+struct ContentView: View {
+    var body : some View {
+        Text("Hello SwiftUI")
+            .font(.title)
+            .foregroundColor(.blue)
+    }
+}
+
+let contentView = ContentView()
+PlaygroundPage.current.setLiveView(contentView)
+
+// # 익스텐션
+// ios SDK (내부 모듈을 확장해서 쓸 수 있다.)
+
+extension Int {
+    var isEven: Bool {
+        return self % 2 == 0
+    }
+}
+
+let number1 = 10
+
+if number1.isEven {
+    print("짝수")
+} else {
+    print("홀수")
+}
